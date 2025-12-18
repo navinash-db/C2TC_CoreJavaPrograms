ed525fad2794cf6fc72e4a143989a625b7a85473
Navinash <db.navinash@gmail.com>
2025-12-18 11:07:20 +0530

    Day 1 and Day 2

---
+++ src/com/tnsif/oops/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.oops.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		OopsConcept obj = new OopsConcept();
+		obj.setSerialNum(7);
+		obj.setName("Navinash");
+		obj.setAge(21);
+		System.out.println(obj);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TNIS_training {
+}
